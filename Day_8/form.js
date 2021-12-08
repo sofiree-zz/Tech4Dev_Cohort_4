551e28adbc8a96057261db4b5087dd9aa20f4da2
sofiree <muosophia0@gmail.com>
2021-12-08 15:17:05 +0100

    Persisted email data using

--- Day_8/form.js
+++ Day_8/form.js
@@ -8,19 +8,12 @@ var EmailField = document.getElementById("email-field");
 var NameField = document.getElementById("name-field");
 var NumberField = document.getElementById("number-field");
 
-if (sessionStorage.getItem("email")){
-    var Email = JSON.parse(sessionStorage.getItem("email"));
-}
-else{
-    alert("HElp")
-}
 
+Email[0].value = sessionStorage.getItem("email")
 
 SubmitButton.addEventListener("submit", (e) => {
-
     e.preventDefault();
     console.log("Udu");
-
     EmailField.innerText = Email[0].value;
     NameField.innerText = Name[0].value;
     NumberField.innerText = PhoneNumber[0].value;
